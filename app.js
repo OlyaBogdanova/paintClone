2fd07f84abe24b52fb458d23a5495fda6a5bdb0b
Olga Bogdanova <bon@incom-sr.ru>
2023-10-18 10:49:16 +0300

    добавляет режим заливки

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const range = document.getElementById("jsRange");
 const btnMode = document.getElementById("jsMode");
 const colors = document.querySelectorAll(".controls__color");
 const ctx = canvas.getContext("2d");
-canvas.height = 700;
-canvas.width = 700;
+const CANVAS_SIZE = 700;
+const INITIAL_COLOR = "#2c2c2c";
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
 ctx.lineWidth = 2.5;
-ctx.strokeStyle = "#2c2c2c";
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+
 let painting = false;
+let filling = false;
 function onMouseMove(e) {
   x = e.offsetX;
   y = e.offsetY;
@@ -32,17 +38,31 @@ function startPainting() {
 }
 
 function changeColor(e) {
-  ctx.strokeStyle = e.target.style.backgroundColor;
+  const color = e.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 function changeRange(e) {
   ctx.lineWidth = +e.target.value;
+}
+
+function handleModeClick() {
+  filling = !filling;
+  btnMode.innerText = filling ? "Рисование" : "Заливка";
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 }
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 colors.forEach((color) => color.addEventListener("click", changeColor));
 range.addEventListener("change", changeRange);
-// btnMode.addEventListener('click', )
+btnMode.addEventListener("click", handleModeClick);
