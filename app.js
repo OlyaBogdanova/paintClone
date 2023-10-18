8a0f30c590cd412fed0f44bc9f5d5784a1b00555
Olga Bogdanova <bon@incom-sr.ru>
2023-10-18 11:01:10 +0300

    добавляет функцию сохранения нарисованной картинки на компьютер

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const range = document.getElementById("jsRange");
 const btnMode = document.getElementById("jsMode");
+const btnSave = document.getElementById("jsSave");
 const colors = document.querySelectorAll(".controls__color");
 const ctx = canvas.getContext("2d");
 const CANVAS_SIZE = 700;
@@ -8,7 +9,8 @@ const INITIAL_COLOR = "#2c2c2c";
 canvas.height = CANVAS_SIZE;
 canvas.width = CANVAS_SIZE;
 ctx.lineWidth = 2.5;
-
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 
@@ -56,13 +58,26 @@ function handleCanvasClick() {
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
+
+function handleCM(e) {
+  e.preventDefault();
+}
+function handleSave() {
+  const image = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = image;
+  a.download = "export PaintJS";
+  a.click();
+}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 colors.forEach((color) => color.addEventListener("click", changeColor));
 range.addEventListener("change", changeRange);
 btnMode.addEventListener("click", handleModeClick);
+btnSave.addEventListener("click", handleSave);
